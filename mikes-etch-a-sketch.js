51edd563e9be71c3a103292fb9f83066d8a63858
Mike Selmani <mike_selmani@hotmail.com>
2022-02-16 23:44:54 -0500

    fiddled with div creation methods, probably learned somethings, created a div inside body element, but failed to create a div inside a container div element, will continue next iteration

---
+++ mikes-etch-a-sketch.js
@@ -0,0 +1,21 @@
+/*
+function createMenuItem(name) {
+    let li = document.createElement('li');
+    li.textContent = name;
+    return li;
+}
+// get the ul#menu
+const menu = document.querySelector('#menu');
+// add menu item
+menu.appendChild(createMenuItem('Home'));
+menu.appendChild(createMenuItem('Services'));
+menu.appendChild(createMenuItem('About Us'));
+*/
+function createDiv(namenumber1) {
+    let div = document.createElement('div');
+    div.textcontent = namenumber1;
+    return div;
+}
+
+const container = document.querySelector('#container');
+container.appendChild(createDiv("div1"));
