f85c314bea746dc7d8ee69152afea57ad57a57e9
Mike Selmani <mike_selmani@hotmail.com>
2022-02-18 15:11:48 -0500

    finally found my js created div in the console, in the HTML DOM tree. added styling properties to my js created div. researched, learned, and achieved replacement of innerhtml for the text of my div by using div.appendChild( document.createTextNode(quoteHelloclosequote)); instead. gained valuable insight into the Document Object Model Tree. Applied those insights to a penicl and paper chart drawing.

--- mikes-etch-a-sketch.js
+++ mikes-etch-a-sketch.js
@@ -14,8 +14,14 @@ menu.appendChild(createMenuItem('About Us'));
 function createDiv(namenumber1) {
     let div = document.createElement('div');
     div.textcontent = namenumber1;
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "red";
+    div.style.color = "white";
+    div.appendChild( document.createTextNode("Hello"));
     return div;
 }
 
 const container = document.querySelector('#container');
 container.appendChild(createDiv("div1"));
+container.appendChild(createDiv("div2"));
