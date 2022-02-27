f9f255ea8717d46135c6260d0018f5f057236cef
Mike Selmani <mike_selmani@hotmail.com>
2022-02-26 22:32:18 -0500

    added mouseover mouseout eventlistener on querySelector selected div. review of methods for selecting more than 1 div with one function in progress

--- mikes-etch-a-sketch.js
+++ mikes-etch-a-sketch.js
@@ -12,13 +12,14 @@ menu.appendChild(createMenuItem('Services'));
 menu.appendChild(createMenuItem('About Us'));
 */
 function createDiv(namenumber1) {
-    let div = document.createElement('div');
+    let div = document.createElement("div");
     div.textcontent = namenumber1;
     div.style.width = "37.5px";
     div.style.height = "37.5px";
     div.style.background = "red";
     div.style.color = "white";
     div.style.border = "groove";
+    div.classList = "256"; 
     div.appendChild( document.createTextNode("Hello"));
     return div;
 }
@@ -28,15 +29,47 @@ for (var i = 0; i < 256; i++) {
     container.appendChild(createDiv("div"));
 }
 
-// need to find out how to add "id" attribute to JS-built divs //
-/*
-const element = document.querySelector("#box");
 
-element.addEventListener("mouseover", event => {
+document.querySelector("div").addEventListener(
+  "mouseover", function () {
+        
+  document.querySelector("div").style.background = "blue";
+  console.log("mouseover");
+})
+
+document.querySelector("div").addEventListener(
+  "mouseout", function () {
+        
+  document.querySelector("div").style.background = "green";
+  console.log("mouseout");
+})
+
+document.getElementsByClassName("256").addEventListener(
+  "mouseover", function () {
+        
+  document.getElementsByClassName("256").style.background = "blue";
+  console.log("mouseover");
+})
+
+
+// math randomization code for random color selection: color[Math.floor(Math.random() * color.length)]  //
+/*
+
+var dynamicdivs = document.getElementsByTagName("div");
+
+Array.from(dynamicdivs);
+
+console.log(dynamicdivs);
+
+dynamicdivs[1].style.background = "blue";
+*/
+/*
+dynamicdivs.slice(1, 5).addEventListener("mouseover", event => {
   console.log("Mouse in");
+
 });
 
-element.addEventListener("mouseout", event => {
+dynamicdivs.slice(1, 5).addEventListener("mouseout", event => {
   console.log("Mouse out");
 });
 */
