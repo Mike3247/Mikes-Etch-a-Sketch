f041f0d2467b42fe9e92cb1ed05a6f8b43cf5a4f
Mike Selmani <mike_selmani@hotmail.com>
2022-04-01 09:19:56 -0400

    write functioning function for mouseover mouseout color change with addEventListener called against forEach of the256divsArray

--- mikes-etch-a-sketch.js
+++ mikes-etch-a-sketch.js
@@ -22,27 +22,27 @@ function createDiv(namenumber1) {
     div.classList = "256"; 
     div.appendChild( document.createTextNode("Hello"));
     return div;
-}
+};
 
 const container = document.querySelector('#container');
 for (var i = 0; i < 256; i++) { 
     container.appendChild(createDiv("div"));
-}
+};
 
 
 document.querySelector("div").addEventListener(
-  "mouseover", function () {
+  "mouseover", () => {
         
   document.querySelector("div").style.background = "blue";
-  console.log("mouseover");
-})
+  //console.log("mouseover");
+});
 
 document.querySelector("div").addEventListener(
   "mouseout", function () {
         
   document.querySelector("div").style.background = "green";
-  console.log("mouseout");
-})
+  //console.log("mouseout");
+});
 
 /*
 document.querySelectorAll(".256").forEach(item => { 
@@ -58,7 +58,33 @@ const the256divsArray = Array.from(the256divs);
 
 console.log(the256divsArray);
 
-the256divsArray.map()
+the256divsArray.forEach((item) => {
+item.addEventListener("mouseover", () => {
+  item.style.background = "green";
+    //console.log("mouseover")
+  })  ///// trying to figure out how to select call EventListener
+});   ////// against the 256 divs as event targets
+
+the256divsArray.forEach((item) => {
+  item.addEventListener("mouseout", () => {
+    item.style.background = "red";
+      //console.log("mouseout")
+    })  
+  });  
+
+
+// the256divsArray.forEach(alert);
+// the256divsArray.forEach((item, index, array) => {
+//   alert(`${item} is at index ${index} in ${array}`);
+// });
+
+// the256divsArray.map() // trying to figure out how 
+                      //to call the array.map 
+//function to iterate through each item of the 
+//array and return ?an event listener?
+// mayber forEach is a more adequate
+// or works and map does not for this
+// I dont know yet
 
 // addEventListener(
 //   "mouseover", function () {
